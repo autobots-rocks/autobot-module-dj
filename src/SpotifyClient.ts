934c6a8b022131e579c9ed6c1e295ba15a43634a
Matthew Davis <matthew@matthewdavis.io>
2019-06-10 14:39:44 -0500

    bump

--- src/SpotifyClient.ts
+++ src/SpotifyClient.ts
@@ -4,7 +4,7 @@ export class SpotifyClient {
 
     public static async search(token: string, type: string, terms: string) {
 
-        request.get(this.getHeaders('search?q=' + terms + '&type=' + type, token), (error, response, body) => {
+        request.get(this.getHeaders('search?q=' + terms + '&type=' + type, token), (error: any, response: any, body: any) => {
 
             console.log(error);
             console.log(response);
